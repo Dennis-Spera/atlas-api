2d1652981c5c6ea0e0598a8e2f81c8f0e843ef7f
dennis.spera <dennis.spera@gmail.com>
2026-08-30 22:14:38 -0400

    fix: load curl credentials from config

--- controlPlane_curl.sh
+++ controlPlane_curl.sh
@@ -58,7 +58,3 @@ curl --user "$ATLAS_PUBLIC_KEY:$ATLAS_PRIVATE_KEY" \
   --digest --include \
   --header "Accept: application/vnd.atlas.$ATLAS_API_VERSION+json" \
   --request GET "https://cloud.mongodb.com/api/atlas/v2/unauth/controlPlaneIPAddresses?pretty=true"
-curl --user "ldccslle:9fd730e2-4869-4f01-b553-195a9b807c60" \
-  --digest --include \
-  --header "Accept: application/vnd.atlas.2025-03-12+json" \
-  --request GET "https://cloud.mongodb.com/api/atlas/v2/unauth/controlPlaneIPAddresses?pretty=true"
